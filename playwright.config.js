f1f3e3c16629f1e49666c870b902f92593c59c3e
iVishalNayak <42730507+iVishalNayak@users.noreply.github.com>
2025-03-08 16:39:50 +0530

    Add files via upload

--- playwright.config.js
+++ playwright.config.js
@@ -5,8 +5,9 @@ export default defineConfig({
   reporter: [['html', { outputFolder: 'playwright-report' }]], // Enable HTML Report
   use: {
     screenshot: 'only-on-failure', // Capture screenshots if a test fails
-    trace: 'on-first-retry', // Capture traces for debugging on retries
+    trace: 'on', // Capture traces for debugging on retries
     headless: false,  // Run in headed mode
-    slowMo: 100000      // Slow down actions (1000ms = 1 sec per step)
+    slowMo: 1000,     // Slow down execution to see steps
+    video: 'on',      // Record video of test execution
   }
 });
